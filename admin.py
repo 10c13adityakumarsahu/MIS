1d52d0e1d4180f77b108db6e84e68e05c1ef31d8
10c13adityakumarsahu <10c13adityakumarsahu@gmail.com>
2024-08-05 02:17:17 +0530

    added stringVars

--- admin.py
+++ admin.py
@@ -22,8 +22,10 @@ F2= LabelFrame(win,text="Login as",bg=backcolour2,relief="solid",border="2",fg=f
 F2.place(x=0,y=80,relwidth=1,height=350)
 #======================Captcha===========================
 captcha=random.randint(0,99999)
-
-    
+#=====================Variables==========================
+passwordvar=StringVar
+registrationvar=IntVar
+captchavar=IntVar
 #======================Students Window===================
 def studentwin():
     #win.destroy()
@@ -41,20 +43,20 @@ def studentwin():
     F5.grid(row=0,column=0,pady=2)
     F6=Label(F4,text="Password",font=style,background=backcolour,foreground=front,anchor="nw",width=13,border="2")
     F6.grid(row=1,column=0,pady=2)
-    F7=Entry(F4,width=15,font=style)
+    F7=Entry(F4,width=15,font=style,textvariable=registrationvar)
     F7.grid(row=0,column=1,pady=2)
-    F8=Entry(F4,width=15,font=style)
+    F8=Entry(F4,width=15,font=style,textvariable=passwordvar)
     F8.grid(row=1,column=1,pady=2)
     F9=Label(F4,text="Enter Captcha",font=style,background=backcolour,foreground=front,anchor="nw",width=13,border="2")
     F9.grid(row=2,column=0,pady=2)
     F10=Label(F4,text=captcha,font=style,background=backcolour,foreground=front,anchor="nw",width=13,border="2")
     F10.grid(row=3,column=0)
     Copy=Label(lay,text="COPYRIGHTS ABC INSTITUTE OF TECHNOLOGY",font=("timesnewroman",10,"bold"),border="2")
-    F11=Entry(F4,width=15,font=style)
+    F11=Entry(F4,width=15,font=style,textvariable=captchavar)
     F11.grid(row=3,column=1)
-    Verify=Button(F4,text="Reset",font=("timesnewroman",10,"bold"),width=25)
+    Verify=Button(F4,text="Reset",font=("timesnewroman",10,"bold"),width=27)
     Verify.grid(row=5,column=0,pady=10)
-    Submit=Button(F4,text="Login",font=("timesnewroman",10,"bold"),width=25)
+    Submit=Button(F4,text="Login",font=("timesnewroman",10,"bold"),width=27)
     Submit.grid(row=5,column=1,pady=10)
     Copy.pack(fill=X,anchor="s",side="bottom")
     lay.bind('<Escape>',quit)
